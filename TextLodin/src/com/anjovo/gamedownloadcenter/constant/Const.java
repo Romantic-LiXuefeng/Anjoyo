d48584fbe905c60c580566aad0ca17fa7284bb46
Administrator <Administrator@192.168.191.5>
2014-11-19 21:14:47 +0800

    11/19-21:14

--- TextLodin/src/com/anjovo/gamedownloadcenter/constant/Const.java
+++ TextLodin/src/com/anjovo/gamedownloadcenter/constant/Const.java
@@ -1,8 +1,13 @@
 package com.anjovo.gamedownloadcenter.constant;
 
 public class Const {
-	//游戏专题的URL
-	public static final String GAME_SPECIAL="http://www.gamept.cn/yx_zt.php?currentPage=";
-	public static final String GAME_SPECIAL_NAME="ztname";
-	public static final String GAME_SPECIAL_IMG="ztimg";
+	// 游戏专题的URL
+	public static final String GAME_SPECIAL = "http://www.gamept.cn/yx_zt.php?currentPage=";
+	public static final String GAME_SPECIAL_NAME = "ztname";
+	public static final String GAME_SPECIAL_IMG = "ztimg";
+	// 签到记录url
+	public static final String SIGNINRECORD_URL = "http://www.gamept.cn/yx_sign.php?uid=163&type=list";
+	public static final String SIGNINRECORD_DATA = "date";
+	public static final String SIGNINRECORD_USERNAME = "username";
+	public static final String SIGNINRECORD_ITEMS = "items";
 }
--- TextLodin/src/com/anjovo/gamedownloadcenter/fragment/SignInRecordFragment.java
+++ TextLodin/src/com/anjovo/gamedownloadcenter/fragment/SignInRecordFragment.java
@@ -1,18 +1,36 @@
 package com.anjovo.gamedownloadcenter.fragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.anjovo.gamedownloadcenter.adapter.MySignInRecordListViewAdapter;
+import com.anjovo.gamedownloadcenter.bean.SignInRecordBean;
+import com.anjovo.gamedownloadcenter.constant.Const;
 import com.anjovo.gamedownloadcenter.fragment.base.TitleFragmentBase;
 import com.anjovo.textlodin.R;
+import com.google.gson.JsonObject;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 
 public class SignInRecordFragment extends TitleFragmentBase {
 	private View rootView;
 	private ListView mListView;
+	private MySignInRecordListViewAdapter mAdapter;
+	private List<SignInRecordBean> mList;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,8 +49,50 @@ public class SignInRecordFragment extends TitleFragmentBase {
 	}
 
 	private void setAdapter() {
-		MySignInRecordListViewAdapter mAdapter = new MySignInRecordListViewAdapter();
+		mList = new ArrayList<SignInRecordBean>();
+		mAdapter = new MySignInRecordListViewAdapter(mList, getActivity());
 		mListView.setAdapter(mAdapter);
+		getSignInRecoed();
+	}
+
+	private void getSignInRecoed() {
+		new HttpUtils().send(HttpMethod.GET, Const.SIGNINRECORD_URL,
+				new RequestCallBack<String>() {
+
+					@Override
+					public void onFailure(HttpException arg0, String arg1) {
+						Toast.makeText(getActivity(), "获取数据失败!请检查网络连接!", 1)
+								.show();
+
+					}
+
+					@Override
+					public void onSuccess(ResponseInfo<String> arg0) {
+						String result = arg0.result;
+						try {
+							JSONObject object = new JSONObject(result);
+							JSONArray jsonArray = object
+									.getJSONArray(Const.SIGNINRECORD_ITEMS);
+							for (int i = 0; i < jsonArray.length(); i++) {
+								JSONObject JSONobject = (JSONObject) jsonArray
+										.get(i);
+								String data = JSONobject
+										.getString(Const.SIGNINRECORD_DATA);
+								String username = JSONobject
+										.getString(Const.SIGNINRECORD_USERNAME);
+								SignInRecordBean bean = new SignInRecordBean(
+										data, username);
+								mList.add(bean);
+								mAdapter.notifyDataSetChanged();
+							}
+
+						} catch (JSONException e) {
+							e.printStackTrace();
+						}
+
+					}
+				});
+
 	}
 
 	@Override
