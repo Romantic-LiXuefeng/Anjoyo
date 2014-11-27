fcb79f09dde79bc323135ba82e655e0e18d2be77
Anjovo <1195063924@qq.com>
2014-11-27 21:09:36 +0800

    gg

--- TextLodin/src/com/anjovo/gamedownloadcenter/activity/HuntActivity.java
+++ TextLodin/src/com/anjovo/gamedownloadcenter/activity/HuntActivity.java
@@ -1,24 +1,26 @@
 package com.anjovo.gamedownloadcenter.activity;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import com.anjovo.textlodin.R;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.ImageButton;
-
-public class HuntActivity extends Activity {
+public class HuntActivity extends Activity implements OnClickListener {
 	/**
 	 * 搜索界面
 	 * 1个返回按钮,1个清空按钮,1个搜索按钮,1个editText,自定义云标签(未实现)
 	 */
 	@ViewInject(R.id.imbtn_back_hunt)
-	private ImageButton mBtnBack;
+	private ImageView mBtnBack;
 	@ViewInject(R.id.imbtn_clear_hunt)
-	private ImageButton mBtnClear;
+	private ImageView mBtnClear;
 	@ViewInject(R.id.imbtn_hunt_hunt)
-	private ImageButton mBtnHunt;
+	private ImageView mBtnHunt;
 	@ViewInject(R.id.et_hunt)
 	private EditText mEtHunt;
 
@@ -26,5 +28,18 @@ public class HuntActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hunt);
+		
+		initView();
+	}
+
+	private void initView() {
+		mBtnBack.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if(v == mBtnBack){
+			this.finish();
+		}
 	}
 }
